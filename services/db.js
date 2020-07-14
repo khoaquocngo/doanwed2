5271eee73328c06c6102a36d41cafdfde74a46d9
NguyenDuongHuy <ndhuy2711@gmail.com>
2020-07-14 14:54:30 +0700

    Design AccountNumber and Tranfers

--- services/db.js
+++ services/db.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize')
 
-const connectionString = process.env.DATABASE_URL || "postgres://postgres:Minhkhoa21@localhost:5432/doanwed";
+const connectionString = process.env.DATABASE_URL || "postgres://postgres:271199@localhost:5432/todo";
 
 const db = new Sequelize(connectionString);
 module.exports = db;
