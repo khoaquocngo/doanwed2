2429bc1ba3dfa405d93b3d1da716d9db2bb9c3d9
NguyenDuongHuy <ndhuy2711@gmail.com>
2020-07-28 23:04:20 +0700

    update design

--- services/db.js
+++ services/db.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize')
-const connectionString = process.env.DATABASE_URL || 'postgres://postgres:Minhkhoa21@localhost:5432/Doanwed';
+const connectionString = process.env.DATABASE_URL || 'postgres://postgres:271199@localhost:5432/todo';
 
 const db = new Sequelize(connectionString);
 module.exports = db;
