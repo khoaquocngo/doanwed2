f0f5a262bcc5db8303831b73ab14acba1f0454b7
NguyenDuongHuy <ndhuy2711@gmail.com>
2020-08-27 00:26:55 +0700

    abc

---
+++ routes/manage/changePass.js
@@ -0,0 +1,30 @@
+const {Router} = require('express');
+const router = new Router();
+const User = require('../services/user');
+const asyncHandler = require('express-async-handler');
+
+router.use(require('../middlewares/requireLoggedIn'));
+
+router.get('/', asyncHandler (async function (req, res) {
+    
+    res.render('manage/changePass');
+
+}));
+
+router.post('/',asyncHandler (async function(req,res) {
+    const user = await User.findUserById(req.currentUser.id);
+    const {passwordOld,passwordNew1,passwordNew2} = req.body;
+    if (passwordNew1 != passwordNew2) {
+        return res.redirect('/changePass');
+    }
+    if (!User.verifyPassword(passwordOld,user.password)) {
+        return res.redirect('/changePass');
+    }
+    user.password = User.hassPassword(passwordNew1);
+    user.save();
+    
+    return res.redirect('/logout');
+
+}));
+
+module.exports = router;
