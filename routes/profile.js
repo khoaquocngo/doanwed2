ca9092f57cf4e1d7a41976735afd0b8f1494f918
Khoa Quốc Ngô <nmkhoa17ck1@gmail.com>
2020-08-28 13:35:57 +0700

    update

--- routes/profile.js
+++ routes/profile.js
@@ -14,11 +14,8 @@ router.get('/', asyncHandler (async function (req, res) {
 router.post('/',asyncHandler (async function (req, res ,next) {
     
     const {imageCMND,displayName,CMND} = req.body;
-    console.log(CMND);
     req.currentUser.CMND = CMND;
-    if(req.currentUser.pictureCMND) {
     req.currentUser.codeCMND = 1;
-    }
     req.currentUser.pictureCMND = imageCMND;
     req.currentUser.displayName = displayName;
     req.currentUser.save();
